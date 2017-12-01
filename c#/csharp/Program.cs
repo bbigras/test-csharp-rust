bb2b9a7730239fa604dbe52bb8e53dcfdf96910f
Bruno Bigras <bigras.bruno@gmail.com>
2017-12-01 16:25:15 -0500

    remove the argument

--- c#/csharp/Program.cs
+++ c#/csharp/Program.cs
@@ -5,18 +5,12 @@ namespace csharp
 {
     class Program
     {
-        static byte[] NullTerminatedUTF8bytes(string str)
-        {
-            var encoding = new UTF8Encoding();
-            return encoding.GetBytes(str + "\0");
-        }
-
         [DllImport("rust.dll")]
-        static extern void test([In] byte[] channel);
+        static extern void test();
 
         static void Main(string[] args)
         {
-            test(NullTerminatedUTF8bytes("123"));
+            test();
         }
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,4 @@
-extern crate libc;
-
-use libc::c_char;
-
 #[no_mangle]
-pub extern "C" fn test(v1: *const c_char) {
+pub extern "C" fn test() {
     println!("test");
 }
